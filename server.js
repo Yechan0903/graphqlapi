903e75fb645bbb9ca2d5f5ba36e54b6f81f45cd8
Yechan0903 Shin <oolo9876@naver.com>
2023-12-05 03:51:03 +0000

    Day1205-1

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 
 import { ApolloServer, gql } from "apollo-server";
 
+let tweets = [
+    {
+        id: "1",
+        text: "first one!",
+    },
+    {
+        id: "2",
+        text: "second one!",
+    },
+]
+
+let users = [
+    {
+        id:"1",
+        firstName:"yechan",
+        lastName:"shin",
+    },
+    {
+        id:"2",
+        firstName:"yeji",
+        lastName:"hang",
+    },
+];
+
 const typeDefs = gql`
     type User {
         id: ID!
-        username: String!
         firstName: String!
-        lastName: String
+        lastName: String!
+        fullName: String!
     }
     type Tweet {
         id: ID!
@@ -14,6 +38,7 @@ const typeDefs = gql`
         author: User!
     }
     type Query {
+        allUser: [User!]!
         allTweets: [Tweet!]!
         tweet(id: ID!): Tweet
     }
@@ -23,7 +48,42 @@ const typeDefs = gql`
     }
 `;
 
-const server = new ApolloServer({typeDefs});
+const resolvers = {
+    Query: {
+        allTweets() {
+            return tweets;
+        },
+        allUser() {
+            return users;
+        },
+        tweet(root, {id}){
+            return tweets.find(t => t.id === id);
+        },
+    },
+    Mutation: {
+        postTweet(_, {text, userId}) {
+            const newTweet= {
+                id: tweets.length + 1,
+                text,
+            };
+            tweets.push(newTweet);
+            return newTweet;
+        },
+        deleteTweet(_, {id}) {
+            const tweet = tweets.find(t => t.id === id);
+            if(!tweet) return false;
+            tweets = tweets.filter(t => t.id !== id);
+            return true;
+        },
+    },
+    User: {
+        fullName({firstName,lastName}) {
+            return `${firstName} ${lastName}`;
+        },
+    },
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
 
 server.listen().then(({url}) => {
     console.log(`Running on ${url}`);
